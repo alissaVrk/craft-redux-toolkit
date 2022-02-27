bc34044d005969cf46cd61838ca30216d935e746
Alissa <alissa.vrk@gmail.com>
2022-02-27 08:52:12 +0200

    mui checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
+import { createTheme, ThemeProvider } from '@mui/material';
+import { Checkbox } from 'components/Checkbox';
 import { Login } from 'features/auth';
 import { ItemsPage, CardsList } from 'features/craft-items';
 import { WorkspaceSelect } from 'features/workspace';
 import './App.css';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0063ff"
+    },
+    grey: {
+      500: "#acacac"
+    },
+    action: {
+      hoverOpacity: 0.35
+    }
+  },
+  components: {
+    MuiCheckbox: {
+      defaultProps: {
+        disableFocusRipple: true,
+        disableRipple: true,
+        disableTouchRipple: true,
+        size: "small"
+      }
+    }
+  }
+});
+
 function App() {
-  return (
-    <div className="App">
-      <Login />
-      <WorkspaceSelect />
-      <CardsList />
-    </div>
-  );
+  return <div> 
+    <ThemeProvider theme={theme}>
+      <Checkbox />
+    </ThemeProvider>
+  </div>
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Login />
+//       <WorkspaceSelect />
+//       <CardsList />
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,52 @@
+import MuiCheckbox, { CheckboxProps } from "@mui/material/Checkbox";
+import { styled } from "@mui/material/styles"
+
+const CheckBoxIcon = styled('div')<{ checked?: boolean }>(({ theme, checked = false }) => {
+    const palette = theme.palette;
+    return {
+        border: `1px solid`,
+        borderColor: !checked ? palette.grey[500] : palette.primary.main,
+        backgroundColor: !checked ? palette.background.paper : palette.primary.main,
+        borderRadius: 2,
+        boxSizing: "border-box",
+        transition: "border .3s,background .3s",
+        display: "inline-flex",
+        alignItems: "center",
+        '&[font-size="small"]': {
+            width: 15,
+            height: 15
+        },
+        "&:after": {
+            content: '""',
+            opacity: !checked ? 0 : '1 !important',
+            color: !checked ? palette.grey[900] : `${palette.common.white} !important`,
+            transition: 'opacity .3s',
+            border: '1px solid',
+            borderTop: 'none',
+            borderRight: 'none',
+            width: 8,
+            height: 4,
+            transform: 'rotate(-50deg)',
+            marginBottom: 2,
+            marginLeft: 1
+        },
+        'input:hover ~ &': {
+            borderColor: palette.primary.main
+        },
+        'input:hover ~ &:after': {
+            opacity: palette.action.hoverOpacity
+        }
+    }
+});
+
+export function Checkbox(props: CheckboxProps) {
+    return <MuiCheckbox
+        disableFocusRipple
+        disableRipple
+        disableTouchRipple
+        icon={<CheckBoxIcon />}
+        checkedIcon={<CheckBoxIcon checked />}
+        size="small"
+        {...props}
+    />
+}
